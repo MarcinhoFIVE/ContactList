fbaa91f8e4aec3702d143f0510b5fd452bda440d
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-20 19:53:52 -0300

    Menu Inserted

--- app/src/main/java/br/mdan/contactlist/MainActivity.kt
+++ app/src/main/java/br/mdan/contactlist/MainActivity.kt
@@ -2,6 +2,14 @@ package br.mdan.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,10 +22,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindView()
         updateList()
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawLayout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun bindView() {
@@ -40,5 +59,29 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Item Menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Item Menu 1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
